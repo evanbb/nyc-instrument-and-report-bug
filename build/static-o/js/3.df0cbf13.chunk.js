300fd5e2470832dffb6d0ffba63f5f16b5be338f
Evan Bremer <evan.bremer@chrobinson.com>
2019-06-04 13:54:18 -0500

    swap dirs back

--- build/static-o/js/3.df0cbf13.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{13:function(n,i,o){"use strict";o.r(i),i.default=function(){return"i am lazy"}}}]);
-//# sourceMappingURL=3.df0cbf13.chunk.js.map
